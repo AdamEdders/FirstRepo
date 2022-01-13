b492a6b29253de117a0bc989469a1729bf6188e4
Adam Edders <adamedders59@gmail.com>
2022-01-13 14:18:43 -0600

    Hello World!

--- Hello World/src/MyPackage/Driver.java
+++ Hello World/src/MyPackage/Driver.java
@@ -4,21 +4,6 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		/* JDK - Java Development Kit
-		 * has all stuff a programmer may need (compiler, base libraries)
-		 * 
-		 * JRE - Java Runtime Environment
-		 * minimum download to run java programs
-		 * can install w/o JDK
-		 * 
-		 * JVM - Java Virtual Machine
-		 * compiles bytecode in realtime and makes code compatible with other JVMs
-		 * actual software that runs the code
-		 * 
-		 * javac - Java Compiler
-		 * turns .java files into .class files known as bytecode
-		 * then JVM consumes .class files during runtime
-		 */
-
+		
 	}
 }
